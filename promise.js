cffda57fc1dc31908b9cc691b0d1e1547dd0649c
Artem Veremienko <temaspsxxx@gmail.com>
2021-09-05 16:56:57 +0300

    style: format files

--- promise.js
+++ promise.js
@@ -1,65 +1,65 @@
 const statuses = {
-	pending: "PENDING",
-	fulfilled: "FULFILLED",
-	rejected: "REJECTED",
-};
+	pending: 'PENDING',
+	fulfilled: 'FULFILLED',
+	rejected: 'REJECTED',
+}
 
 class MyPromise {
-	#status;
-	#thenFn = () => {};
-	#catchFn = () => {};
+	#status
+	#thenFn = () => {}
+	#catchFn = () => {}
 
 	constructor(fn) {
-		this.#status = statuses.pending;
-		fn(this.#resolve.bind(this), this.#reject.bind(this));
+		this.#status = statuses.pending
+		fn(this.#resolve.bind(this), this.#reject.bind(this))
 	}
 
 	#resolve(data) {
 		if (this.#status === statuses.pending) {
-			this.#status = statuses.fulfilled;
+			this.#status = statuses.fulfilled
 			setTimeout(() => {
 				try {
-					this.#thenFn(data);
+					this.#thenFn(data)
 				} catch (e) {
-					this.#status = statuses.rejected;
-					this.#catchFn(e);
+					this.#status = statuses.rejected
+					this.#catchFn(e)
 				}
-			});
+			})
 		}
 	}
 
 	#reject(err) {
 		if (this.#status === statuses.pending) {
-			this.#status = statuses.rejected;
-			setTimeout(() => this.#catchFn(err));
+			this.#status = statuses.rejected
+			setTimeout(() => this.#catchFn(err))
 		}
 	}
 
 	then(onResolved, onRejected) {
 		if (onResolved) {
-			this.#thenFn = onResolved;
+			this.#thenFn = onResolved
 		}
 		if (onRejected) {
-			this.#catchFn = onRejected;
+			this.#catchFn = onRejected
 		}
-		return this;
+		return this
 	}
 
 	catch(onRejected) {
-		return this.then(null, onRejected);
+		return this.then(null, onRejected)
 	}
 }
 
 const promiseTimeout = new MyPromise((resolve, reject) => {
 	setTimeout(() => {
-		resolve("Time is over");
-		reject(new Error("I'm error"));
-	}, 1000);
-});
+		resolve('Time is over')
+		reject(new Error("I'm error"))
+	}, 1000)
+})
 
 promiseTimeout
 	.then((data) => {
-		console.log(data);
-		throw new Error("Error from then!");
+		console.log(data)
+		throw new Error('Error from then!')
 	})
-	.catch((err) => console.log(err.message || err));
+	.catch((err) => console.log(err.message || err))
